d46421081f8c4ad31637092fac81e0015d23e89c
teyyub <teyyub392@gmail.com>
2022-01-20 17:17:35 +0400

    test stok to database

--- trade-portfolio/src/main/java/com/payday/bank/repository/OrderRepository.java
+++ trade-portfolio/src/main/java/com/payday/bank/repository/OrderRepository.java
@@ -8,11 +8,19 @@ import com.payday.bank.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 /**
  * 
- * @author David Ferreira Pinto
+ * @author anar
  *
  */
 public interface OrderRepository extends CrudRepository<Order,Integer> {
 
 	List<Order> findByAccountId(String accountId);
 	Order findByOrderId(Integer orderId);
+
+	"select * from (SELECT accountid, initial_quantity,symbol,completiondate,'buying' as type FROM ORDERS "
+	"where completiondate>=sysdate-60 "
+	"union all "
+	"select accountid,quantity,symbol,completiondate,'selling' as type from sells "
+	"where completiondate>=sysdate-60) bb "
+	"order by bb.type,bb.completiondate asc "
+
 }
